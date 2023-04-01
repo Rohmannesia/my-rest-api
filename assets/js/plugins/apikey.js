2beca96f4afdad0394128423ba368fcc453029b0
Rohmannesia <128954737+Rohmannesia@users.noreply.github.com>
2023-04-02 06:07:15 +0700

    Update apikey.js

--- assets/js/plugins/apikey.js
+++ assets/js/plugins/apikey.js
@@ -27,7 +27,7 @@
           } else {
             textnya.textContent = '[ INFO ] Apikey anda tidak valid!'
             swal("Apikey Tidak Valid!", "", "error");
-            alert('Apikey Tidak Valid. Silahkan beli apikey ke Instagram @rohmanzx_offici');
+            alert('Apikey Tidak Valid. Silahkan beli apikey ke Instagram @rohmanzx_official');
             amlert.classList.remove('hideop')
           }
         };
